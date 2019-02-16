6cbd1ec729691219e7d0e3c4a27defab30dc871d
Jared Diehl <jareddiehl96@gmail.com>
2019-02-16 01:53:36 -0600

    Added handlebars

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
 const express = require('express');
+const hbs = require('express-handlebars');
 const path = require('path');
+const fs = require('fs');
 const process = require('process');
+const bodyParser = require('body-parser');
 
 const app = express();
 const server = require('http').Server(app);
 
-const PORT = process.env.port || 8000;
+const PORT = process.env.port || 8080;
 const HOST = process.env.host || 'localhost';
 const ENV = app.get('env');
 
-app.use(express.static(path.join(__dirname, 'assets')));
+app.engine('hbs', hbs({
+  extname: '.hbs',
+  defaultLayout: 'main',
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials/'
+}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+  console.log('testing');
+  res.render('index', {
+    title: 'Paint the Town'
+  });
+});
+
+app.post('/test', (req, res) => {
+  console.log('testing');
 });
 
 server.listen(PORT, HOST, () => {
