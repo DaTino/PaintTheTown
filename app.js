eed4437954023c182c6a88e5b96f393993f12b88
DaTino <36283321+DaTino@users.noreply.github.com>
2019-02-17 00:14:39 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const hbs = require('express-handlebars');
+const http = require('http');
+const https = require('https');
 const path = require('path');
 const fs = require('fs');
 const process = require('process');
@@ -8,14 +10,23 @@ const config = require('./config');
 const googleMapsClient = require('@google/maps').createClient({
   key: 'AIzaSyAStlYQh66ZsHEE9OUqT1KXo9VC8t3TEyM'
 });
+const nodeGeocoder = require('node-geocoder');
+const ngOptions = {
+  provider: 'google',
+  httpAdapter: 'https', // Default
+  apiKey: 'AIzaSyAStlYQh66ZsHEE9OUqT1KXo9VC8t3TEyM', // for Mapquest, OpenCage, Google Premier
+  formatter: null        // 'gpx', 'string', ...
+};
+const geocoder = nodeGeocoder(ngOptions);
+
 
 var albertoKey = 'AIzaSyAStlYQh66ZsHEE9OUqT1KXo9VC8t3TEyM';
 var jaredKey = 'AIzaSyCCuO6urauhG_XFJvRRwet5r7_kpPBd6Cw';
 
 const app = express();
-const server = require('http').Server(app);
+const server = http.Server(app);
 
-const PORT = process.env.port || 8001;
+const PORT = process.env.port || 8080;
 const HOST = process.env.host || 'localhost';
 const ENV = app.get('env');
 
@@ -37,79 +48,42 @@ app.get('/', (req, res) => {
 });
 
 app.post('/map', (req, res) => {
-  var key = "AIzaSyAStlYQh66ZsHEE9OUqT1KXo9VC8t3TEyM"
+  var key = "AIzaSyAStlYQh66ZsHEE9OUqT1KXo9VC8t3TEyM";
    //var data = req.body;
-   var age = "21" //data.selectAge;
-   var outing = "date night" //data.selectOut;
-   var budget = "2" //data.selectBudget;
-   var location = "St. Louis, Missouri" //data.selectLocation;
-   var radius = "16090" //data.selectRadius * 1609 //meters conversion;
+   //var age = data.selectAge;
+   var outing = "date"; //data.selectOut;
+   var budget = "2"; //data.selectBudget;
+   var location = "Philadelphia, PA"; //data.selectLocation;
+   var radius = 16090; //data.selectRadius * 1609 //meters conversion
 
-   var https = require('https');
-   var url = "http://maps.googleapis.com/maps/api/place/nearbysearch/json?"
-    + "key=" + key + "&location=" + location + "&radius=" + radius
-    + "&keyword=" + outing + "&maxbudget" + budget;
-    console.log(url);
-    https.get(url, function(response) {
-        var body ='';
-        response.on('data', function(chunk) {
-          body += chunk;
+   geocoder.geocode(location, function(err, res) {
+     var latitude = res[0].latitude;
+     var longitude= res[0].longitude;
+     console.log(latitude);
+     console.log('here');
+     var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
+      + "key=" + key  + "&location=" + latitude + "," + longitude + "&radius=" + radius;
+      //+ "&keyword=" + outing + "&maxbudget=" + budget;
+      console.log(url);
+      https.get(url, function(response) {
+          var body ='';
+          console.log('cock');
+          response.on('data', function(chunk) {
+            body += chunk;
+          });
+        console.log('piss');
+        response.on('end', function() {
+          var places = JSON.parse(body);
+          var locations = places.results;
+          console.log(locations);
+          res.json(locations);
         });
-
-      response.on('end', function() {
-        var place = JSON.parse(body);
-        var locations = places.results;
-        console.log(locations);
-        res.json(locations);
+      }).on('error', function() {
+          console.log("Got error: I broke" );
       });
-    }).on('error', function() {
-        console.log("Got error: I broke" );
-    });
+   });
 
-   //api stuff called
-   /*
-   https://maps.googleapis.com/maps/api/place/nearbysearch/output?parameters
-   key
-   locations
-   radius
-   maxprice
-   keyword
-   rankby=prominence
-
-   */
-
-   res.render('redirect');
-});
-
-app.get('/titties', (req, res) => {
-  var key = 'AIzaSyAStlYQh66ZsHEE9OUqT1KXo9VC8t3TEyM';
-  var location = encodeURIComponent(req.query.location);
-  var radius = 16000;
-  var sensor = false;
-  var types = "restaurant";
-  var keyword = "fast";
-
-  var https = require('https');
-  var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + "key=" + key + "&location=" + location + "&radius=" + radius + "&sensor=" + sensor + "&types=" + types + "&keyword=" + keyword;
-    console.log(url);
-  https.get(url, function(response) {
-    var body ='';
-    response.on('data', function(chunk) {
-      body += chunk;
-    });
-
-    response.on('end', function() {
-      var places = JSON.parse(body);
-      var locations = places.results;
-      var randLoc = locations[Math.floor(Math.random() * locations.length)];
-
-      console.log(places);
-      console.log(locations);
-      res.json(randLoc);
-    });
-  }).on('error', function(e) {
-    console.log("Got error: " + e.message);
-  });
+   res.render('map');
 });
 
 app.use((req, res, next) => {
